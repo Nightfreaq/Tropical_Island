366d09a962d8bd18d9f8d79d083b63bac9705e8e
Michał~ <mi.kielbik@gmail.com>
2025-08-28 22:35:14 +0200

    nav

--- js/script.js
+++ js/script.js
@@ -1,34 +1,27 @@
+const nav = document.querySelector(".nav");
+const bars = document.querySelector(".burger-btn");
+const navLinks = document.querySelectorAll(".nav__item");
 
-const nav = document.querySelector('.nav')
-const bars = document.querySelector('.burger-btn')
-const navLinks = document.querySelectorAll('.nav__item')
+const navShow = () => {
+nav.classList.toggle('nav--active')
 
-const handleNav = () => {
-  nav.classList.toggle('nav--active')
-
-  navLinks.forEach(link =>{
-    link.addEventListener('click', () =>{
-      nav.classList.remove('nav--active')
-    })
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('nav--active')
   })
-
-  navAnimation ();
+})
+navAnimation ()
 }
 
-const navAnimation = () => {
+const navAnimation = () =>{
+let = delayTime = 0
 
-  let = delayTime = 0;
-
-  navLinks.forEach(link => {
-    link.classList.toggle('links-animation')
-    link.style.animationDelay = '.' + delayTime + 's'
-    delayTime++
-
-    if(link.classList.contains('links-animation')){
-      link.classList.remove('links-animation')
-    }
-  })
-
+navLinks.forEach( link => {
+  link.classList.toggle('links-animation')
+  link.style.animationDelay = '.' + delayTime + 's'
+  delayTime++
+})
 }
 
-bars.addEventListener('click', handleNav)
+bars.addEventListener('click', navShow)
+
