8730e8296f6dc8aa325d58e8331225950faab1ba
Michał~ <mi.kielbik@gmail.com>
2025-09-19 14:23:22 +0200

    footer year js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const nav = document.querySelector(".nav");
 const bars = document.querySelector(".burger-btn");
 const navLinks = document.querySelectorAll(".nav__item");
 
+const btnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll(".section")
+const footerYear = document.querySelector('.footer__year')
+
 const navShow = () => {
 nav.classList.toggle('nav--active')
 
@@ -25,3 +29,9 @@ navLinks.forEach( link => {
 
 bars.addEventListener('click', navShow)
 
+const handleCurrentYear = () => {
+  const year = (new Date).getFullYear();
+  footerYear.innerText = year;
+}
+
+handleCurrentYear();
