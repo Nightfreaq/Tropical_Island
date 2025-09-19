bddedf413c8d45d0de70a69c7d7effe53dc5d9cb
Michał~ <mi.kielbik@gmail.com>
2025-09-19 14:46:06 +0200

    bars js

--- js/script.js
+++ js/script.js
@@ -2,36 +2,46 @@ const nav = document.querySelector(".nav");
 const bars = document.querySelector(".burger-btn");
 const navLinks = document.querySelectorAll(".nav__item");
 
-const btnBars = document.querySelector('.burger-btn__bars')
-const allSections = document.querySelectorAll(".section")
-const footerYear = document.querySelector('.footer__year')
+const btnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const navShow = () => {
-nav.classList.toggle('nav--active')
+	nav.classList.toggle("nav--active");
 
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.remove('nav--active')
-  })
-})
-navAnimation ()
-}
+	navLinks.forEach((link) => {
+		link.addEventListener("click", () => {
+			nav.classList.remove("nav--active");
+		});
+	});
+	navAnimation();
+};
 
-const navAnimation = () =>{
-let = delayTime = 0
+const navAnimation = () => {
+	let = delayTime = 0;
 
-navLinks.forEach( link => {
-  link.classList.toggle('links-animation')
-  link.style.animationDelay = '.' + delayTime + 's'
-  delayTime++
-})
-}
+	navLinks.forEach((link) => {
+		link.classList.toggle("links-animation");
+		link.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
 
-bars.addEventListener('click', navShow)
+bars.addEventListener("click", navShow);
 
 const handleCurrentYear = () => {
-  const year = (new Date).getFullYear();
-  footerYear.innerText = year;
-}
-
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
 handleCurrentYear();
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach((section) => {
+
+    if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+      btnBars.classList.add('black-bars-color')
+    }
+
+  });
+};
